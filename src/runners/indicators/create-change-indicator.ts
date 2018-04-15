f09a75df3ac51b83870a70c1ea59450b96865719
Tom Manion <thomas.w.manion@accenture.com>
2018-04-15 14:33:16 -0500

    Adding pop indicator to change run

--- src/runners/indicators/create-change-indicator.ts
+++ src/runners/indicators/create-change-indicator.ts
@@ -1,3 +1,4 @@
+import { PopularityIndicator } from './../../popularity/popularity.indicator';
 import { VolatilityIndicator } from './../../volatility/volatility.indicator';
 import { dateUtil } from '../../util/date.util';
 import { HistoricalChangeUpdater } from '../../historical-changes/historical-change.updater';
@@ -5,8 +6,10 @@ import { HistoricalChangeUpdater } from '../../historical-changes/historical-cha
 export async function createChangeIndicator() {
   const updater = new HistoricalChangeUpdater();
   await updater.createChangeIndicatorsForDate(dateUtil.today);
-  const volatilityIndicator = new VolatilityIndicator();
-  await volatilityIndicator.createVolatilityIndicatorsForDate(dateUtil.today);
+  const popIndicator = new PopularityIndicator();
+  await popIndicator.createPopularityIndicatorsForDate(dateUtil.today);
+  // const volatilityIndicator = new VolatilityIndicator();
+  //await volatilityIndicator.createVolatilityIndicatorsForDate(dateUtil.today);
 }
 createChangeIndicator().catch(e => {
   console.error(e);
