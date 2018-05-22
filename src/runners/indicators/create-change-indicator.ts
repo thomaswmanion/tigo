ba9d31376d7666e9838ba08013f187f58bd27136
Tom Manion <thomas.w.manion@accenture.com>
2018-05-22 17:39:55 -0500

    Fixing change indicator

--- src/runners/indicators/create-change-indicator.ts
+++ src/runners/indicators/create-change-indicator.ts
@@ -1,12 +1,15 @@
+import { VolatilityIndicator } from './../../volatility/volatility.indicator';
 import { PopularityIndicator } from './../../popularity/popularity.indicator';
 import { dateUtil } from '../../util/date.util';
-import { HistoricalChangeUpdater } from '../../historical-changes/historical-change.updater';
+import { DirectionIndicator } from '../../direction/direction.indicator';
 
 export async function createChangeIndicator() {
-  const updater = new HistoricalChangeUpdater();
-  await updater.createChangeIndicatorsForDate(dateUtil.today);
   const popIndicator = new PopularityIndicator();
   await popIndicator.createPopularityIndicatorsForDate(dateUtil.today);
+  const dirIndicator = new DirectionIndicator();
+  await dirIndicator.createDirectionIndicatorsForDate(dateUtil.today);
+  const volIndicator = new VolatilityIndicator();
+  await volIndicator.createVolatilityIndicatorsForDate(dateUtil.today);
 }
 createChangeIndicator().catch(e => {
   console.error(e);
